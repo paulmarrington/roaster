150be3034cdaeed9be823828cd6185bdd0c58c8d
Paul Marrington <paul@marrington.net>
2013-03-16 13:38:04 +1100

    moved from emitter back to (err, module) pattern

--- server/demand.js
+++ server/demand.js
@@ -1,29 +1,23 @@
 // Copyright (C) 2012,13 Paul Marrington (paul@marrington.net), see uSDLC2/GPL for license
-var path = require('path'), util = require('util')
+var path = require('path')
 var prefix = path.join(process.env.uSDLC_node_path, "ext")
-var EventEmitter = require('events').EventEmitter
 
 // Load npm module if new, otherwise behave as require does
-
-var Demand = function(name, on_load) {
-  EventEmitter.call(this)
-  var required
-  try {
-    required = require(name)
-  } catch(error) {
-    var npm = require("ext/node/lib/node_modules/npm")
-    npm.load({prefix: prefix}, function(err, npm) {
-      try {
-        npm.commands.install([name], 
-          function() { required = require(name) })
-      } catch(error) {
-        this.emit('error', error)
+module.exports = function(name, on_load) {
+    var required
+    try {
+        required = require(name)
+    } catch(error) {
+        var npm = require("ext/node/lib/node_modules/npm")
+        npm.load({prefix: prefix}, function(err, npm) {
+            try {
+                npm.commands.install([name], function() { callback(null, require(name)) })
+            } catch(error) {
+                on_load(error)
+                return
+            }
+        })
         return
-      }
-    })
-  }
-    on_load(required)
+    }
+    on_load(null, required)
 }
-util.inherits(Demand, EventEmitter);
-
-module.exports = function(module, on_load) { new Demand(module, on_load) }
