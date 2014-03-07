93fe0078e198481571a4713ee062fe9131b004a5
Paul Marrington <paul@marrington.net>
2014-03-07 18:11:25 +1000

    make install windows friendly

--- release/update-node-on-unix.sh
+++ release/update-node-on-unix.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Copyright (C) 2012,13 Paul Marrington (paul@marrington.net), see uSDLC2/GPL for license
-base=$(cd "`dirname "${BASH_SOURCE[0]}"`" && pwd)
+# Copyright (C) 2012,13 paul@marrington.net, see /GPL license
+base=$(cd $(dirname "$0"); pwd)
 echo
 
 ##################################################
 # change this as new versions of node are released
 ##################################################
-nodeVersion=v0.10.25
+nodeVersion=v0.10.26
 
 ################################################
 # Now we download the correct version of node-js
@@ -14,30 +14,42 @@ nodeVersion=v0.10.25
 mkdir "$base/../ext" 2>/dev/null
 cd "$base/../ext"
 
+function unpack() { # for unix-like systems
+  echo "unpack $1"
+  tar -xzf $1
+  dirname=${1%.tar.gz}
+  mv $dirname node
+}
+
 os="`uname`-`uname -m`"
 case $os in
-	Darwin-x86_64) os=darwin-x64 ;;
-	Darwin-i386) os=darwin-x86 ;;
-	Linux-x86_64) os=linux-x64 ;;
-	Linux-i386) os=linux-x86 ;;
+	Darwin-x86_64) fn=node-$nodeVersion-darwin-x64.tar.gz ;;
+	Darwin-i386) fn=node-$nodeVersion-darwin-x86.tar.gz ;;
+	Linux-x86_64) fn=node-$nodeVersion-linux-x64.tar.gz ;;
+	Linux-i386) fn=node-$nodeVersion-linux-x86.tar.gz ;;
+    Windows*) fn=node.exe
+     ar=node-$nodeVersion-windows-x86.exe
+     function unpack() {
+       echo "prepare $ar"
+       mkdir node
+       mkdir node/bin
+       cp node.exe node/bin
+       mv node.exe $ar
+     }
+     ;;
 	*) echo "Unknown OS version - '$os'"
 	   echo "Compile from source at 'http://nodejs.org/dist/$nodeVersion/node-$nodeVersion.tar.gz'"
 	   exit
 esac
-filename=node-$nodeVersion-$os
-if [ ! -e $filename.tar.gz ]
+if [ ! -e ${ar-$fn} ]
 then
-    echo "download $filename.tar.gz from nodejs.org"
+    echo "download $fn from nodejs.org"
     if hash curl 2>/dev/null; then
-      curl -sOL http://nodejs.org/dist/$nodeVersion/$filename.tar.gz
+      curl -sOL http://nodejs.org/dist/$nodeVersion/$fn
     else
-      wget -N http://nodejs.org/dist/$nodeVersion/$filename.tar.gz
+      wget -N http://nodejs.org/dist/$nodeVersion/$fn
     fi
-    echo "unpack $filename"
-    tar -xzf $filename.tar.gz
     rm -r node 2>/dev/null
-    mkdir node
-    mv $filename/* node
-    rm -r $filename
+    unpack $fn
 fi
 echo
